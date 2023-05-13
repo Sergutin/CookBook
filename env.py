898b24e2bffab2b6aa1a3af1b80bee63277be08f
Sergutin <igor@sergutin.com>
2023-05-13 15:13:12 +0000

    Initial commit

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
-os.environ["DATABASE_URL"]="postgres://tbwjujoz:vhLZOWHjOsVdI12g3QfJWebnabpD_mJe@mel.db.elephantsql.com/tbwjujoz"
+os.environ["DATABASE_URL"]="postgres://xcsdmcjz:AumZAc0OYhwKlFDF5NFUNqFxNR5fSfj5@horton.db.elephantsql.com/xcsdmcjz"
 os.environ["SECRET_KEY"]="!ThisIsMySecret123!"
 os.environ["CLOUDINARY_URL"] = "cloudinary://871135155492683:SrHNEsE7XyOSVafEp3GJ43cIQhg@drgwkbuet"
 os.environ['DEVELOPMENT'] = "1"
