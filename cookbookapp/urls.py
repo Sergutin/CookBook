f8917e5289be822d7fc0fc153f5df00b9fa102e9
Sergutin <igor@sergutin.com>
2023-05-14 18:14:52 +0000

    Change static root

--- cookbookapp/urls.py
+++ cookbookapp/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-
+from cookbookapp import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', views.create_recipe, name='create_recipe'),
     path('edit/<int:pk>/', views.edit_recipe, name='edit_recipe'),
     path('delete/<int:pk>/', views.delete_recipe, name='delete_recipe'),
+    
 ]
