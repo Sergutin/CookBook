08f1c30da00b8391626c650ebec662a6aa95fe8d
Sergutin <igor@sergutin.com>
2023-05-29 17:58:44 +0000

    Add gitignore

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ["DATABASE_URL"]="postgres://xcsdmcjz:AumZAc0OYhwKlFDF5NFUNqFxNR5fSfj5@horton.db.elephantsql.com/xcsdmcjz"
-os.environ["SECRET_KEY"]="!ThisIsMySecret123!"
-os.environ["CLOUDINARY_URL"] = "cloudinary://871135155492683:SrHNEsE7XyOSVafEp3GJ43cIQhg@drgwkbuet"
-# os.environ['DEVELOPMENT'] = "1"
