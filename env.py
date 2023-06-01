55b9cf84c4c188871be026cd3a9c41f9ed6205d0
Sergutin <igor@sergutin.com>
2023-06-01 14:49:21 +0000

    Fix deployment issues

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://cwvaubac:ntlqZpYml9jKa2f5iMNbs_BzsHP17UQ3@lucky.db.elephantsql.com/cwvaubac"
-os.environ["SECRET_KEY"] = "changed!SecretKey777!"
-os.environ["CLOUDINARY_URL"] = "cloudinary://853527214367273:x6bxAZ1vqXbKWm6Y9n1Tmp0WVDo@dvec0etvd"
-# os.environ['DEVELOPMENT'] = "1"
